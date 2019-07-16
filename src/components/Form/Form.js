5f474064368897f6bd11eac47c06455acd154488
MVendrik <35732059+MVendrik@users.noreply.github.com>
2019-07-16 17:15:34 +0200

    Add data to firestore with Form component.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,37 +1,91 @@
-import React from "react";
+import React, { Component } from "react";
+import firebase from "../../Firestore";
 
-const form = () => {
-  const classNames = "active";
+class Form extends Component {
+  initialState = {
+    dataForm: {
+      name: {
+        value: ""
+      },
+      cost: {
+        value: ""
+      }
+    }
+  };
 
-  return (
-    <div className={" indigo"}>
-      <div className={"col s12 m4"}>
-        <form className={"card z-depth-0"}>
-          <div className={"card-content"}>
-            <span className={"card-title indigo-text"}>Add item:</span>
-            <div className={"input-field"}>
-              <input type="text" id="name" />
-              <label for="name" className={classNames}>
-                Item name
-              </label>
+  state = this.initialState;
+
+  inputChangedHandler = (e, id) => {
+    e.persist();
+    const updatedDataForm = { ...this.state.dataForm };
+    const updatedFormElement = { ...updatedDataForm[id] };
+    updatedFormElement.value = e.target.value;
+    updatedDataForm[id] = updatedFormElement;
+    this.setState({ dataForm: updatedDataForm });
+  };
+
+  submitFormHandler = e => {
+    e.preventDefault();
+
+    const item = {
+      name: this.state.dataForm.name.value,
+      cost: this.state.dataForm.cost.value
+    };
+
+    const db = firebase.firestore();
+    db.collection("expenses")
+      .add(item)
+      .then(res => {
+        this.setState(() => this.initialState);
+      });
+  };
+
+  render() {
+    const classNames = "active";
+    return (
+      <div className={" indigo"}>
+        <div className={"col s12 m4"}>
+          <form className={"card z-depth-0"} onSubmit={this.submitFormHandler}>
+            <div className={"card-content"}>
+              <span className={"card-title indigo-text"}>Add item:</span>
+              <div className={"input-field"}>
+                <input
+                  type="text"
+                  id="name"
+                  required
+                  value={this.state.dataForm.name.value}
+                  onChange={e => this.inputChangedHandler(e, "name")}
+                />
+                <label for="name" className={classNames}>
+                  Item name
+                </label>
+              </div>
+              <div className={"input-field"}>
+                <input
+                  type="number"
+                  id="cost"
+                  required
+                  value={this.state.dataForm.cost.value}
+                  onChange={e => this.inputChangedHandler(e, "cost")}
+                />
+                <label for="cost" className={classNames}>
+                  Item Cost ($)
+                </label>
+              </div>
+              <div className={"input-field center"}>
+                <button className={"btn-large pink white-text"}>
+                  Add Item
+                </button>
+              </div>
+              <div className={"input-field center"}>
+                <p className={"red-text"} id="error" />
+              </div>
             </div>
-            <div className={"input-field"}>
-              <input type="number" id="cost" />
-              <label for="cost" className={classNames}>
-                Item Cost ($)
-              </label>
-            </div>
-            <div className={"input-field center"}>
-              <button className={"btn-large pink white-text"}>Add Item</button>
-            </div>
-            <div className={"input-field center"}>
-              <p className={"red-text"} id="error" />
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default form;
+export default Form;
