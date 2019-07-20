4afd91527784ffb46369828cf9e9edaa8f440bca
MVendrik <35732059+MVendrik@users.noreply.github.com>
2019-07-20 19:03:44 +0200

    Added css for fitness tracker.

--- src/containers/FitnessTracker/FitnessTracker.js
+++ src/containers/FitnessTracker/FitnessTracker.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import Header from "../../components/Header/Header";
+import "./FitnessTracker.css";
 
 class FitnessTracker extends Component {
-  state = {};
   render() {
     return (
-      <div>
+      <div className={"grey darken-4 FitnessChartFullPage"}>
         <Header
-          headerClasses={"grey darken-4 section z-depth-0"}
+          headerClasses={"headerFitness section z-depth-0"}
           h2Classes={"center white-text Header"}
           h2Text={"Fitness Chart"}
           pClasses={"flow-text grey-text center text-lighten-2"}
