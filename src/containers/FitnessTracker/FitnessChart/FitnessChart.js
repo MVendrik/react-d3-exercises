1d413e2f7ecba42342124c49c423c32f4975f524
MVendrik <35732059+MVendrik@users.noreply.github.com>
2019-07-23 16:12:29 +0200

    Fetch fitness data in chart component.

--- src/containers/FitnessTracker/FitnessChart/FitnessChart.js
+++ src/containers/FitnessTracker/FitnessChart/FitnessChart.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
 import "./FitnessChart.css";
+import firebase from "../../../Firestore";
 
 class FitnessChart extends Component {
-  state = {};
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    let dataArray = [];
+
+    const db = firebase.firestore();
+    db.collection("activities").onSnapshot(res => {
+      res.docChanges().forEach(change => {
+        const doc = { ...change.doc.data(), id: change.doc.id };
+
+        switch (change.type) {
+          case "added":
+            dataArray.push(doc);
+            break;
+          case "modified":
+            const index = dataArray.findIndex(item => item.id === doc.id);
+            dataArray[index] = doc;
+            break;
+          case "removed":
+            dataArray = dataArray.filter(item => item.id !== doc.id);
+            break;
+          default:
+            break;
+        }
+      });
+      this.setState({ data: dataArray });
+    });
+  }
+
   render() {
     return (
       <div className={"col s12 l6 ChartSection"}>
